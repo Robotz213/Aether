1b11f21c041144281b4fa0a52275b376b3f35a2f
Robotz213 <nicholas@robotz.dev>
2025-10-02 14:28:57 -0400

    Update ruff.toml to ignore additional linting rule and refactor main.py for improved WSGI server execution

--- threaderwsgi/__main__.py
+++ threaderwsgi/__main__.py
@@ -3,7 +3,5 @@
 from threaderwsgi.main import app
 
 __all__ = [app]
-
-
 if __name__ == "__main__":
-    app()
+    app()  # pragma: no cover
--- threaderwsgi/main.py
+++ threaderwsgi/main.py
@@ -1,14 +1,55 @@
+"""ThreaderWSGI main entry point."""
+
+from contextlib import suppress
+from importlib import import_module
+from wsgiref.types import WSGIApplication
+
 from rich import print
 from typer import Typer
+
+from threaderwsgi._threadpoolwsgi import ThreadPoolWSGIServer
 
 app = Typer(name="TheaderWSGI")
 
 
-@app.command(name="teste")
-def teste() -> None:
-    print("ok")
+@app.command(name="run")
+def run(
+    app: str = "app:app",
+    host: str = "localhost",
+    port: int = 5000,
+) -> None:
+    """Executa o servidor WSGI com ThreadPool."""
+    if ":" in app:
+        module_name, app_name = app.split(":", 1)
+        app_module: WSGIApplication = import_module(module_name)
+
+        app_instance = getattr(app_module, app_name)
+
+        if callable(app_instance):
+            app_instance = app_instance()
+
+        ThreadPoolWSGIServer(host=host, port=port, app=app_instance).run()
 
 
-@app.command(name="teste2")
-def teste2() -> None:
-    print("ok")
+@app.command(name="serve", deprecated=True)
+def serve(
+    app: str = "app:app",
+    host: str = "localhost",
+    port: int = 5000,
+) -> None:
+    """Executa o servidor WSGI com ThreadPool."""
+    if ":" in app:
+        module_name, app_name = app.split(":", 1)
+
+        with suppress(ImportError):
+            app_module: WSGIApplication = import_module(module_name)
+
+            app_instance = getattr(app_module, app_name)
+
+            if callable(app_instance):
+                app_instance = app_instance()
+
+            ThreadPoolWSGIServer(host=host, port=port, app=app_instance).run()
+            return
+
+        print(f"[red]Error:[/red] Module '{module_name}' not found.")

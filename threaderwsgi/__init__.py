29d175434a9221c19f5eac7ad1dd1b469acccfd4
Robotz213 <nicholas@robotz.dev>
2025-10-02 12:12:22 -0400

    Update dependencies and enhance app structure with Typer integration

--- threaderwsgi/__init__.py
+++ threaderwsgi/__init__.py
@@ -1,13 +1,21 @@
 """Threaded WSGI Server com graceful shutdown."""
 
-import tqdm
+from tqdm import tqdm
+from typer import Typer
 
 from ._requesthandlerwsgi import WSGIRequestHandler
 from ._threadpoolwsgi import ThreadPoolWSGIServer
 
+app = Typer(name="TheaderWSGI")
+
+
+@app.command()
+def about() -> None:
+    tqdm.write("ok")
+
 
 def _main_function() -> None:
-    tqdm.tqdm.write("ok")
+    return app()
 
 
 __all__ = [ThreadPoolWSGIServer, WSGIRequestHandler, _main_function]

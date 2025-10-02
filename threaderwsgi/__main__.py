dcca289a5884530e5e82086688a3f5807c5d7b6d
Robotz213 <nicholas@robotz.dev>
2025-10-02 12:25:40 -0400

    Refactor entry point to use 'typer_app' and update exports in __main__.py

--- threaderwsgi/__main__.py
+++ threaderwsgi/__main__.py
@@ -1,5 +1,9 @@
 """Empt."""
 
-from threaderwsgi.main import app
+from threaderwsgi.main import typer_app
 
-__all__ = ["app"]
+__all__ = ["typer_app"]
+
+
+if __name__ == "__main__":
+    typer_app()
